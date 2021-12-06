8d16f0d8f04247591bea2cc275a354b9376d4844
YangQi <70502828+yangfong@users.noreply.github.com>
2021-12-06 11:24:40 +0800

    perf：参数修改为可选，优化类型声明

--- src/util/style.ts
+++ src/util/style.ts
@@ -1,44 +1,43 @@
-
-interface size {
-  small: string;
-  default: string;
-  large: string;
+interface Size {
+    small: string;
+    default: string;
+    large: string;
 }
 
-interface commonInterFace {
-  margin: string;
-  position: string;
-  left: number;
-  right: number;
-  top: number;
-  bottom: number;
+interface CommonInterFace {
+    margin: string;
+    position: string;
+    left: number;
+    right: number;
+    top: number;
+    bottom: number;
 }
 
-const commonStyle : commonInterFace = {
-  margin: 'auto',
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0
+const commonStyle: CommonInterFace = {
+    margin: 'auto',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
 };
 
-const sizeItem : size = {
-  small: '10px',
-  default: '12px',
-  large: '14px'
+const sizeItem: Size = {
+    small: '10px',
+    default: '12px',
+    large: '14px'
 }
 
-const sizeContainer : size = {
-  small: '24px',
-  default: '30px',
-  large: '36px'
+const sizeContainer: Size = {
+    small: '24px',
+    default: '30px',
+    large: '36px'
 }
 
-const borderRadiusContainerSize : size = {
-  small: '12px',
-  default: '15px',
-  large: '18px'
+const borderRadiusContainerSize: Size = {
+    small: '12px',
+    default: '15px',
+    large: '18px'
 }
 
-export  { commonStyle, sizeItem, sizeContainer, borderRadiusContainerSize }
+export {commonStyle, sizeItem, sizeContainer, borderRadiusContainerSize}
